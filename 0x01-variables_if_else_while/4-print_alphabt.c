bbdf6e2287e054a9c70d5edfff200fdce2d0f756
agzsoftsi <agzsoftsi@gmail.com>
2019-09-19 19:08:55 +0000

    alphabet lower less e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Success)
+**/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
