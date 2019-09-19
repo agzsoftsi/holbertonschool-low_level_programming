c0202ed7a909d88eba1232343fcfb86de10528c6
agzsoftsi <agzsoftsi@gmail.com>
2019-09-19 19:24:38 +0000

    Numeric 0 to 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,14 @@
 /**
  * main - entry block
  * @void: no argument
- * Return: 0 (Success)
+ * Return: 0
 **/
 int main(void)
 {
 int i;
 
-for (i = 0; i <= 10; i++)
-putchar(c);
-
-putchar('\n');
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
 return (0);
 }
