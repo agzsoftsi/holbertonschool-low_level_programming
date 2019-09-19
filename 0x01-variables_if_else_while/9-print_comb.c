3a16192d3c3e011a07b1c96583752ecf67488147
agzsoftsi <agzsoftsi@gmail.com>
2019-09-19 21:29:30 +0000

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+**/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i % 10 + '0');
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
