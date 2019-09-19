ca79316ed8f965295029796246a18142521865f3
agzsoftsi <agzsoftsi@gmail.com>
2019-09-19 21:46:01 +0000

    count 0 100

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+**/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 100; i++)
+{
+putchar(i / 10 % 10 + '0');
+putchar(i % 10 + '0');
+if (i < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
