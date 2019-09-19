ccfae0cfffe9631c8932509881370846ab332675
agzsoftsi <agzsoftsi@gmail.com>
2019-09-19 18:59:27 +0000

    alphabet lower Upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+**/
+int main(void)
+{
+char c, d;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (d = 'A'; d <= 'Z'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
