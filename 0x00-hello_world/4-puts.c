85ee6444fad650f43f0f3690e1978ad9f6e1b1cc
agzsoftsi <agzsoftsi@gmail.com>
2019-09-19 15:28:06 +0000

    final Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints string
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
