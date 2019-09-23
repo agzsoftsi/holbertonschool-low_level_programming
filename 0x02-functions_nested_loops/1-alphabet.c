a4abe0016f272e2138c4c617c608fed7fb73b88f
agzsoftsi <agzsoftsi@gmail.com>
2019-09-23 22:30:03 +0000

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ * @void: no argument
+**/
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
