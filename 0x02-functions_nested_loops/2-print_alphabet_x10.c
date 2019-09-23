4029acf97411725bef8f952e90d97c0ada0d6e4d
agzsoftsi <agzsoftsi@gmail.com>
2019-09-23 22:37:35 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ * @void - no argument
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
+}
