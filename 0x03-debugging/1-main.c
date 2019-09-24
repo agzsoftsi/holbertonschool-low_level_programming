1739c659be19d1ffd38da6ca4ed5ccec76eaef2f
agzsoftsi <agzsoftsi@gmail.com>
2019-09-24 19:49:40 +0000

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@ int main(void)
   /*
    * int i;
    */
-  printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
   /*
    * i = 0;
    */
@@ -18,7 +18,7 @@ int main(void)
    * putchar(i);
    * }
    */
-  printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
-  return (0);
+return (0);
 }
