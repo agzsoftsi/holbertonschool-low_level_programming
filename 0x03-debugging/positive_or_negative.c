3fafc88c0922d8164a8076aec58f2c9cfe401b2f
agzsoftsi <agzsoftsi@gmail.com>
2019-09-26 14:16:40 +0000

    isupper

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - positive or negative function
+ * @void: no arguments
+ * Return:0
+ **/
+void positive_or_negative(int i)
+{
+
+
+ if (i>0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+}
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_isupper - check for uppercase character
+ *@c:integer
+ *
+ *Return: 1 if c is uppercase, o otherwise
+ */
+
+int _isupper(int c)
+{
+if (c <= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
