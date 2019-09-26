eb3830b7596ee0fc83d30fdd5356d18cd043ad5f
agzsoftsi <agzsoftsi@gmail.com>
2019-09-26 19:33:57 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ *main - print numbers 1 to 100
+ *@void: no arguments
+ *Return: always 0
+**/
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if ((a % 3) == 0 && (a % 5) == 0)
+{
+printf("FizzBuzz ");
+}
+else if ((a % 3) == 0)
+{
+printf("Fizz ");
+}
+else if ((a % 5) == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", a);
+}
+}
+printf("\n");
+return (0);
+}
