ec8a8b5f9b1acd564391f73c2ad344d8c8e32102
agzsoftsi <agzsoftsi@gmail.com>
2019-09-26 14:55:37 +0000

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *print_numbers - prints numbers 0 to 9
+ *@void: no arguments
+ *
+ *Return: always 0
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0 ; a <= 9 ; a++)
+{
+_putchar(a + '0');
+}
+_putchar('\n');
+}
