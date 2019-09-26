8b325539e14fad5ddd54a114a881087476697a7b
agzsoftsi <agzsoftsi@gmail.com>
2019-09-26 20:11:40 +0000

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *print_triangle - print a triangule
+ *@size: integer
+ */
+void print_triangle(int size)
+{
+int x;
+int y;
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+if (y <= size - x)
+_putchar (' ');
+else
+_putchar ('#');
+}
+_putchar('\n');
+}
+}
