b1370cb917d1a531dbcc70ffc5a42eaac3f9a531
agzsoftsi <agzsoftsi@gmail.com>
2019-09-26 21:04:03 +0000

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - finds and prints the larges prime factor of the number
+ *@void: no arguments
+ *Return: zero
+ */
+int main(void)
+{
+long i, num = 612852475143;
+for (i = 2; i <= num; i++)
+{
+if (num % i == 0)
+{
+num = num / i;
+i--;
+}
+}
+printf("%lu\n", i);
+return (0);
+}
