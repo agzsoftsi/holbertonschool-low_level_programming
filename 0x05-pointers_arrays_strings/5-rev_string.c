3703cd652e3ad341af51a2c3d6b3585314cf336c
agzsoftsi <agzsoftsi@gmail.com>
2019-10-01 18:00:30 +0000

    reverse and asks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *rev_string - Reverse a string
+ *@s: takes a string
+ */
+void rev_string(char *s)
+{
+int i, c = 0;
+char temp;
+while (s[c] != '\0')
+c++;
+c--;
+for (i = 0; i < c; c--, i++)
+{
+temp = s[i];
+s[i] = s[c];
+s[c] = temp;
+}
+}
