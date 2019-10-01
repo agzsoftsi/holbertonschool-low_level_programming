e8179dcd33ff9a280ac2126caf018d7c24a89b3a
agzsoftsi <agzsoftsi@gmail.com>
2019-10-01 19:52:09 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *print_array - prints n elements of an array of integers
+ *@a:integer
+ *@n:integer
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
