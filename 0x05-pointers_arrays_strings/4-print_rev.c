984623fb496b9b7037ab83b9a21a9292a50cf3a6
agzsoftsi <agzsoftsi@gmail.com>
2019-10-01 15:28:09 +0000

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include"holberton.h"
+/**
+ *print_rev - print in reverse a string
+ *@s:takes a string of characters
+ */
+void print_rev(char *s)
+{
+int i = 0, c;
+while (s[c] != '\0')
+c++;
+c--;
+for (i = c; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
