8fa3a5a74d67b9415b84568eb0c34814bdd58b6b
agzsoftsi <agzsoftsi@gmail.com>
2019-10-01 20:33:02 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_strcpy - copy the string to by src
+ *@dest: destination
+ *@src: source
+ *Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; *(src + i) != '\0'; i++)
+*(dest + i) = *(src + i);
+return (dest);
+}
