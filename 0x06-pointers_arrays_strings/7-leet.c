fd86ffdc8b70be52640c56d44a4c061f84b2a3cf
agzsoftsi <agzsoftsi@gmail.com>
2019-10-03 21:43:44 +0000

    Mozart

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *leet - encodes a string
+ *@s: string
+ *Return: s
+ */
+char *leet(char *s)
+{
+int i, j;
+char *s1 = "aeotlAEOTL";
+char *s2 = "4307143071";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; s1[j] != '\0'; j++)
+{
+if (s[i] == s1[j])
+s[i] = s2[j];
+}
+}
+return (s);
+}
