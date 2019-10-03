07074a3f10e673fdf0094e63737cedfd9687318f
agzsoftsi <agzsoftsi@gmail.com>
2019-10-03 18:47:01 +0000

    Reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *reverse_array - reverse an array
+ *@a: array
+ *@n: integer
+ */
+void reverse_array(int *a, int n)
+{
+int i, temp = 0;
+n = n - 1;
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - i];
+a[n - i] = temp;
+}
+}
