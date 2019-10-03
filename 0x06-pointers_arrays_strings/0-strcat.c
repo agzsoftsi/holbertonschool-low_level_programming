c9b9d89eab529d722f1ac78f037bad3450bf8830
agzsoftsi <agzsoftsi@gmail.com>
2019-10-03 17:35:52 +0000

    pointers2

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *char *_strcat - concatenates two strings
+ *@dest: Destination of the new string
+ *@src: Source of the string
+ *Return: Return dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j;
+while (dest[i] != '\0')
+i++;
+
+for (j = 0; src[j] != '\0'; j++)
+dest[i + j] = src[j];
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strncpy - copy a string
+ *@dest: destination string
+ *@src: source of string
+ *@n: max byte of src
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+int j;
+while (src[i] != '\0')
+i++;
+for (j = 0; j < n && src[j] != '\0'; j++)
+dest[j] = src[j];
+for (; n > i; i++)
+dest[i] = '\0';
+
+return (dest);
+}
