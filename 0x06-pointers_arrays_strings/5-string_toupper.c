da244377ed043a4404d299f9cf249aaaa337783f
agzsoftsi <agzsoftsi@gmail.com>
2019-10-03 19:18:09 +0000

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *string_toupper - changes all lowercase by uppercase
+ *@s: string
+ *Return: s
+ *
+ */
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+}
+return (s);
+}
