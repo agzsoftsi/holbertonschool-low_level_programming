1d6a35b23359b63333afc830069f2a046543cf48
agzsoftsi <agzsoftsi@gmail.com>
2019-10-03 23:32:03 +0000

    fix

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,37 +1,29 @@
-#include <stdio.h>
 #include "holberton.h"
-
 /**
- * rot13 - rot13 encoding
- * @s: string
- *
- * Return: string with rot13;
+ * rot13 - encodes string to rot13
+ * @s: takes in a string
+ * Return: s, the string
  */
-
 char *rot13(char *s)
 {
-int i;
-i = 0;
+int i, j;
 
-while (s[i] != '\0')
+char *s1 = "abcdefghijklmABCDEFGHIJKLM";
+char *s2 = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] >= 65 && s[i] <= 77)
+for (j = 0; s1[j] != '\0'; j++)
 {
-s[i] = s[i] + 13;
+if (s[i] == s1[j])
+{
+s[i] = s2[j];
 }
-else if (s[i] >= 78 && s[i] <= 90)
+else if (s[i] == s2[j])
 {
-s[i] = s[i] - 13;
+s[i] = s1[j];
 }
-else if (s[i] >= 97 && s[i] <= 109)
-{
-s[i] = s[i] + 13;
 }
-else if (s[i] >= 98 && s[i] <= 122)
-{
-s[i] = s[i] - 13;
-}
-i++;
 }
 return (s);
 }
