193b88e5778cade5f13acec95a41440f134c20a2
agzsoftsi <agzsoftsi@gmail.com>
2019-10-07 21:04:36 +0000

    fix

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_chessboard - prints the chessboard
+ *@a: char
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+int x, y;
+for (x = 0; x < 8; x++)
+{
+for (y = 0; y < 8; y++)
+putchar(a[x][y]);
+putchar('\n');
+}
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *print_diagsums
+ *@a: integer
+ *@size: integer
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+
+
+
+}
