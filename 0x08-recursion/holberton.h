894fa0c8dc0309417be57e43e4bd24a6ded702b4
agzsoftsi <agzsoftsi@gmail.com>
2019-10-10 16:43:44 +0000

    PLDfix

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,6 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt2(int a, int b);
 int is_prime_number(int n);
+int prime2(int x, int y);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 #endif
