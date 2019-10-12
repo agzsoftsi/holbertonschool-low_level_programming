b6865cde315035116da4b044eea6e25088408f3c
agzsoftsi <agzsoftsi@gmail.com>
2019-10-12 21:20:51 +0000

    fix

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins to make change money
+ * @argc: count
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int coin;
+int can;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+coin = atoi(argv[1]);
+can = 0;
+while (coin > 0)
+{
+if (coin >= 25)
+{
+coin -= 25;
+can += 1;
+}
+else if (coin >= 10)
+{
+coin -= 10;
+can += 1;
+}
+else if (coin >= 5)
+{
+coin -= 5;
+can += 1;
+}
+else if (coin >= 2)
+{
+coin -= 2;
+can += 1;
+}
+else
+{
+coin -= 1;
+can += 1;
+}
+}
+printf("%d\n", can);
+return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds adds positive numbers
+ * @argc: count
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+int can1, can2, numbers;
+
+for (can1 = 1; can1 < argc; can1++)
+{
+for (can2 = 0; argv[can1][can2] != '\0'; can2++)
+{
+if (!isdigit(argv[can1][can2]))
+{
+printf("Error\n");
+return (1);
+}
+}
+numbers = numbers + atoi(argv[can1]);
+}
+printf("%i\n", numbers);
+return (0);
+}
