39d46b9240005c3bc7b493b912c4eea92fa347b4
agzsoftsi <agzsoftsi@gmail.com>
2019-10-13 01:06:24 +0000

    fix

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * @void: void
+ * Return: 0
+ */
+int main(void)
+{
+long int second = 2, first = 1, sum = 0, con;
+
+printf("%lu, ", first);
+printf("%lu, ", second);
+
+for (con = 3; con < 49; con++)
+{
+sum = first + second;
+first = second;
+second = sum;
+if (con < 48)
+{
+printf("%lu, \n", sum);
+}
+else
+{
+printf("%lu\n", sum);
+}
+}
+return (0);
+}
