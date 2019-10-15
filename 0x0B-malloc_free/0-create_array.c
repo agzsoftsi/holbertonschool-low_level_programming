af6d0d74247b579acce651a2854c31f3a0de7fc8
agzsoftsi <agzsoftsi@gmail.com>
2019-10-15 17:38:27 +0000

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *create_array - Create an array of chars
+ *@size: size of string
+ *@c: new string
+ *Return: NULL if is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int con;
+char *parray;
+if (size == 0)
+return (NULL);
+else
+parray = malloc(size * sizeof(c));
+for (con = 0; con < size; con++)
+*(parray + con) = c;
+return (parray);
+}
