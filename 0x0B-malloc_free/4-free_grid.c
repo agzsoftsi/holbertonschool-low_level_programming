8865c6ec7145c2e75eb654e582970f7c1889ed28
agzsoftsi <agzsoftsi@gmail.com>
2019-10-15 22:22:22 +0000

    Task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid previously created by your alloc_grid function
+ * free_grid - frees a 2 dimensional grid previously created by
+ * your alloc_grid function
  * @grid: array
  * @height: height of array
  */
