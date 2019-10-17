82acca2705512c9ff1bc523abec3408e06448e74
agzsoftsi <agzsoftsi@gmail.com>
2019-10-17 18:42:25 +0000

    Malloc task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers
+ * @min: integer mininum
+ * @max: integer maximum
+ * Return: creates an array of elements
+ * If min > max, return NULL
+ * If malloc fails, return NULL
+ */
+int *array_range(int min, int max)
+{
+int *lon;
+int con1, con2;
+
+if (min > max)
+return (NULL);
+
+lon = malloc(sizeof(int) * (max - min + 1));
+if (lon == NULL)
+return (NULL);
+
+for (con1 = min, con2 = 0; con1 <= max; con1++, con2++)
+lon[con2] = con1;
+return (lon);
+}
