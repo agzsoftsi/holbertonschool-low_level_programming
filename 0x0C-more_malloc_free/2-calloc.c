99f8c988124db156fae3ff5355b992bee28ede6c
agzsoftsi <agzsoftsi@gmail.com>
2019-10-17 18:32:44 +0000

    Malloc task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,29 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int con;
-int *array;
+void *array;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
 array = malloc(nmemb * size);
 if (array == 0)
 return (NULL);
-for (con = 0; con < size; con++)
-array[con] = 0;
+
+_memset(array, 0, nmemb * size);
 return (array);
+}
+
+/**
+ * _memset - Fill constant byte
+ * @mem: memory area to fill
+ * @cons: constant byte
+ * @num: number of bytes to fill
+ * Return: Pointer to the memory area s
+ */
+char *_memset(char *mem, char cons, unsigned int num)
+{
+unsigned int con;
+for (con = 0; con < num; con++)
+mem[con] = cons;
+return (mem);
 }
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -4,6 +4,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strl(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *mem, char cons, unsigned int num);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
