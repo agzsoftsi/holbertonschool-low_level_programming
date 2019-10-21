74fcf659a7d5b420f91ce747547744471e301aaf
agzsoftsi <agzsoftsi@gmail.com>
2019-10-21 20:14:59 +0000

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,9 +11,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 char *nname, *nowner;
 dog_t *ndog;
-if (name == NULL || owner == NULL)
-return (NULL);
-
+/*
+ *if (name == NULL || owner == NULL)
+ *return (NULL);
+ */
 ndog = malloc(sizeof(dog_t));
 if (ndog == NULL)
 return (NULL);
