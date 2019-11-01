2cbcd7440d9cee5ad54bd3070cf4d56ec4086042
agzsoftsi <agzsoftsi@gmail.com>
2019-11-01 20:16:26 +0000

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * free_list - free memory
+ * @head: pointer to type list_t
+ */
+void free_list(list_t *head)
+{
+list_t *move;
+
+while (head != NULL)
+{
+move = head;
+head = move->next;
+free(move->str);
+free(move);
+}
+}
