f2aad926401170ab203c70ffdf8cb65ec3deda62
agzsoftsi <agzsoftsi@gmail.com>
2019-12-16 23:47:26 +0000

    100 advance

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - add two numbers
+ * @a: num1
+ * @b: num2
+ * Return: add of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: num1
+ * @b: num2
+ * Return: difference of two numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: num1
+ * @b: num2
+ * Return: product of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: num1
+ * @b: num2
+ * Return: quotient of two numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulo two numbers
+ * @a: num1
+ * @b: num2
+ * Return: modulouseous of two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
