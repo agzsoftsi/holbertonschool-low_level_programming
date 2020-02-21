d94ef4f2220f14a79b9ff3567294c15e4973d5e4
agzsoftsi <agzsoftsi@gmail.com>
2020-02-21 02:06:02 +0000

    Task 9 Done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,20 @@
  * main - entry block
  * @void: no argument
  * Return: 0
-**/
+ **/
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 0; i < 10; i++)
-{
-putchar(i % 10 + '0');
-if (i < 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
