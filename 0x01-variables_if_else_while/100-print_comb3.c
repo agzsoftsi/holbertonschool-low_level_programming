8d927d672420a6d256f064da0a7d13c7ec10a431
agzsoftsi <agzsoftsi@gmail.com>
2020-02-21 02:07:23 +0000

    Task 100 Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,24 +3,24 @@
  * main - entry block
  * @void: no argument
  * Return: 0
-**/
+ **/
 int main(void)
 {
-int i, j;
+	int i, j;
 
-for (i = '0'; i <= '9'; i++)
-{
-for (j = i + 1; j <= '9'; j++)
-{
-putchar(i);
-putchar(j);
-if (i != '8' || j != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
