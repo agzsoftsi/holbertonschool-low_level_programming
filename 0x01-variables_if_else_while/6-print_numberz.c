498233c2ea8e59031794c0c8cbcff6c5c882a520
agzsoftsi <agzsoftsi@gmail.com>
2020-02-21 02:03:28 +0000

    Task 6 Done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,16 @@
  * main - entry block
  * @void: no argument
  * Return: 0
-**/
+ **/
 int main(void)
 {
-int i = 0;
+	int i = 0;
 
-while (i < 10)
-{
-putchar(i % 10 + '0');
-i++;
-}
-putchar('\n');
-return (0);
+	while (i < 10)
+	{
+		putchar(i % 10 + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
