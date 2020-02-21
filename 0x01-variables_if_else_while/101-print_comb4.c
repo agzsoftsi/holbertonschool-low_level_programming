616f92f56221bf6b3e5b20e70330f918ec3c1313
agzsoftsi <agzsoftsi@gmail.com>
2020-02-21 02:08:03 +0000

    Task 101 Done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,28 +3,28 @@
  * main - entry block
  * @void: no argument
  * Return: 0
-**/
+ **/
 int main(void)
 {
-int i, j, k;
+	int i, j, k;
 
-for (i = '0'; i <= '9'; i++)
-{
-for (j = i + 1; j <= '9'; j++)
-{
-for (k = j + 1; k <= '9'; k++)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (i != '7' || j != '8' || k != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || j != '8' || k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
