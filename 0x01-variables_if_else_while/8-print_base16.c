7a8c051e0e50758025c4641c5e9a1028157b3f60
agzsoftsi <agzsoftsi@gmail.com>
2020-02-21 02:05:18 +0000

    Task 8 Done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,16 +3,16 @@
  * main - entry block
  * @void: no argument
  * Return: 0
-*/
+ */
 int main(void)
 {
-int i;
-char c;
+	int i;
+	char c;
 
-for (i = 0; i < 10; i++)
-putchar(i % 10 + '0');
-for (c = 'a'; c < 'g'; c++)
-putchar(c);
-putchar('\n');
-return (0);
+	for (i = 0; i < 10; i++)
+		putchar(i % 10 + '0');
+	for (c = 'a'; c < 'g'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
