714772e41772dfa620b517f87335d6f9dcf07cc2
agzsoftsi <agzsoftsi@gmail.com>
2020-02-21 02:01:26 +0000

    Task 4 Done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,16 +3,16 @@
  * main - entry block
  * @void: no argument
  * Return: 0 (Success)
-**/
+ **/
 int main(void)
 {
-char c;
+	char c;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-if (c != 'e' && c != 'q')
-putchar(c);
-}
-putchar('\n');
-return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
