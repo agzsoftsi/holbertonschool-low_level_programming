49cc7dc81a1fc5fca2b8abcad3e0885b0d039a18
agzsoftsi <agzsoftsi@gmail.com>
2020-02-21 02:09:36 +0000

    Task 102 Done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,27 +3,27 @@
  * main - entry block
  * @void: no argument
  * Return: 0
-**/
+ **/
 int main(void)
 {
-int i, j;
+	int i, j;
 
-for (i = 0; i < 100; i++)
-{
-for (j = i + 1; j < 100; j++)
-{
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
