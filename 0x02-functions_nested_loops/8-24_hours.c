02a9bbdcb1d6ea55466f770574fdb7a1d3efa961
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:48:19 +0000

    Task 8 Done - Identation fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,18 @@
  */
 void jack_bauer(void)
 {
-int i, j;
+	int i, j;
 
-for (i = 0; i < 24; i++)
-{
-for (j = 0; j < 60; j++)
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-_putchar(':');
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
-_putchar('\n');
-}
-}
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
