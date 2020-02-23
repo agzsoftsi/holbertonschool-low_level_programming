4c377d4aecdb4f7b2c997bb09bbfcbf367413e80
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:50:21 +0000

    Task 11 Done - Identation fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,30 +7,30 @@
  */
 void print_to_98(int n)
 {
-int i;
+	int i;
 
-if (n > 98)
-{
-for (i = n; i > 97; i--)
-{
-printf("%d", i);
-if (i != 98)
-{
-printf(", ");
-}
-}
-}
-else
-{
-for (i = n; i < 99; i++)
-{
-printf("%d", i);
-if (i != 98)
-{
-printf(", ");
-}
-}
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i < 99; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
 
-}
-printf("\n");
+	}
+	printf("\n");
 }
