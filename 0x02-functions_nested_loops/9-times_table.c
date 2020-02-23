362be32fdac97068a3d3b76d74e0bc647758de5b
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:48:57 +0000

    Task 9 Done - Identation fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,32 @@
  */
 void times_table(void)
 {
-int i, j, k;
+	int i, j, k;
 
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-k = i * j;
-if (j == 0)
-{
-_putchar(k + '0');
-}
-else if (k <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(k / 10 + '0');
-_putchar(k % 10 + '0');
-}
-}
-_putchar('\n');
-}
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			else if (k <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
