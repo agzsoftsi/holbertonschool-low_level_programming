75ba9608e3938ad9e047debff407a25499370239
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:51:52 +0000

    Task 101 Done - Identation fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int n;
-int sum = 0;
+	int n;
+	int sum = 0;
 
-for (n = 0; n < 1024; n++)
-{
-if (n % 3 == 0 || n % 5 == 0)
-{
-sum += n;
-}
-}
-printf("%d\n", sum);
-return (0);
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
