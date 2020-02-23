3560367ef72daad1442affaabd2471edc88b8973
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:41:23 +0000

    Task 0 Done - Identation fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,17 +3,17 @@
  * main - entry block
  * @void: no argument
  * Return: 0
-**/
+ **/
 int main(void)
 {
-char holberton[] = "Holberton";
+	char holberton[] = "Holberton";
 
-int c;
+	int c;
 
-for (c = 0; c < 9; c++)
-{
-_putchar(holberton[c]);
-}
-_putchar('\n');
-return (0);
+	for (c = 0; c < 9; c++)
+	{
+		_putchar(holberton[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
