88033009bd5c6484355268e00ce6fcbf0bce63e2
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:44:35 +0000

    Task 3 Done - Identation fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,12 +6,12 @@
  */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
