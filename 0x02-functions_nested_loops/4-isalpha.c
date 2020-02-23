8eb09afb5733371d70a8d36b8e77b1a6d4e081b4
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:45:31 +0000

    Task 4 Done - Identation fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,12 +6,12 @@
  */
 int _isalpha(int c)
 {
-if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
