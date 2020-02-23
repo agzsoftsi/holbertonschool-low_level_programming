a60e1302fcd38bbff4d7bf695d2a06b6f753ae00
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:47:42 +0000

    Task 7 Done - Identation fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,10 @@
  */
 int print_last_digit(int n)
 {
-int ln = n % 10;
+	int ln = n % 10;
 
-if (n < 0)
-ln = ln * -1;
-_putchar(ln + '0');
-return (ln);
+	if (n < 0)
+		ln = ln * -1;
+	_putchar(ln + '0');
+	return (ln);
 }
