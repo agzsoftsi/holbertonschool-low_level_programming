1cdd2149e0e2000fb558b908779528ee1d563132
agzsoftsi <agzsoftsi@gmail.com>
2020-02-23 02:53:09 +0000

    Task 103 Done - Identation fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-long pri = 0, sec = 1, sum, total_sum;
-int val;
+	long pri = 0, sec = 1, sum, total_sum;
+	int val;
 
-sum = pri + sec;
-while (sum <= 4000000)
-{
-val = sum % 2;
-sum = pri + sec;
-if (val == 0)
-{
-total_sum += sum;
-}
-pri = sec;
-sec = sum;
-}
-printf("%lu\n", total_sum);
-return (0);
+	sum = pri + sec;
+	while (sum <= 4000000)
+	{
+		val = sum % 2;
+		sum = pri + sec;
+		if (val == 0)
+		{
+			total_sum += sum;
+		}
+		pri = sec;
+		sec = sum;
+	}
+	printf("%lu\n", total_sum);
+	return (0);
 }
