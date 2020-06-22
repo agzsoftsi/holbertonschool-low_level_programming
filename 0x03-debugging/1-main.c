e077bcb772e3bed04e6e26332747459a4c44aef4
agzsoftsi <agzsoftsi@gmail.com>
2020-06-22 23:42:23 +0000

    Codes Indented

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,20 +5,20 @@
  */
 int main(void)
 {
-  /*
-   * int i;
-   */
-printf("Infinite loop incoming :(\n");
-  /*
-   * i = 0;
-   */
-  /*
-   * while (i < 10)
-   * {
-   * putchar(i);
-   * }
-   */
-printf("Infinite loop avoided! \\o/\n");
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,16 +8,16 @@ void positive_or_negative(int i)
 {
 
 
- if (i>0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
 }
