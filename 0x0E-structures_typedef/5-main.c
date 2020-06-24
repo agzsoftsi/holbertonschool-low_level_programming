4d43e91fe8e69d581d66a19c11a30964ae30daa9
agzsoftsi <agzsoftsi@gmail.com>
2020-06-24 03:09:34 +0000

    Codes Indented

--- 0x0E-structures_typedef/5-main.c
+++ 0x0E-structures_typedef/5-main.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-dog_t *my_dog;
+	dog_t *my_dog;
 
-my_dog = new_dog("Django", 3.5, "Jay");
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-free_dog(my_dog);
-return (0);
+	my_dog = new_dog("Django", 3.5, "Jay");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	free_dog(my_dog);
+	return (0);
 }
