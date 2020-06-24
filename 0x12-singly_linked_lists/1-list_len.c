10ac953c6cc4f83ccd8183430e1d2c380446c4cf
agzsoftsi <agzsoftsi@gmail.com>
2020-06-24 14:08:44 +0000

    Codes Indented

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 /**
-* list_len -  prints all the elements of a list_t list
-* @h: element of the structure
-* Return: number of nodes
-*/
+ * list_len -  prints all the elements of a list_t list
+ * @h: element of the structure
+ * Return: number of nodes
+ */
 
 size_t list_len(const list_t *h)
 {
-size_t con = 0;
-const list_t *move = h;
+	size_t con = 0;
+	const list_t *move = h;
 
-if (h == NULL)
-return (0);
+	if (h == NULL)
+		return (0);
 
-while (move != NULL)
-{
-move = move->next;
-con++;
-}
-return (con);
+	while (move != NULL)
+	{
+		move = move->next;
+		con++;
+	}
+	return (con);
 }
