8ab03e1af632e231f6070dbf6a2399b6c34d964f
agzsoftsi <agzsoftsi@gmail.com>
2020-06-24 20:48:22 +0000

    Codes Indented

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -9,14 +9,14 @@
  */
 int main(int ac, char **av)
 {
-int res;
+	int res;
 
-if (ac != 3)
-{
-dprintf(2, "Usage: %s filename text\n", av[0]);
-exit(1);
-}
-res = create_file(av[1], av[2]);
-printf("-> %i)\n", res);
-return (0);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -9,14 +9,14 @@
  */
 int main(int ac, char **av)
 {
-int res;
+	int res;
 
-if (ac != 3)
-{
-dprintf(2, "Usage: %s filename text\n", av[0]);
-exit(1);
-}
-res = append_text_to_file(av[1], av[2]);
-printf("-> %i)\n", res);
-return (0);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = append_text_to_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
